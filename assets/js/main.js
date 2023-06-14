aae3cc8ebe8a1dea1e83dca952c18f950f16687f
Mihir Shah <mihir.shah@tatvamail.com>
2023-06-14 15:33:17 +0530

    chatjs svg animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 module.exports = {
     jsPath: [
         'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        // 'node_modules/chart.js/dist/chart.umd.js',
         'node_modules/aos/dist/aos.js',
+        'assets/js/chartCode.js',
         'assets/js/custom.js',
     ]
 };
